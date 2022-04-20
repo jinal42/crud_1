a8115057b585de236b9b42f42c59b3b17dc9bd33
jinal42 <jij.globaliasoft@gmail.com>
2022-04-20 16:46:53 +0530

    msg

---
+++ enroll/views.py
@@ -0,0 +1,43 @@
+import email
+from django.shortcuts import render,HttpResponseRedirect
+from requests import request
+
+from enroll.models import User
+from .forms import StudentReg
+
+# Create your views here.
+
+# insert and display data :-
+def add_show(request):
+      if request.method == 'POST':
+            fm = StudentReg(request.POST)
+            # if fm.is_valid():                # 1st method
+            #  fm.save()
+
+            if fm.is_valid():                  # 2nd method
+              nm = fm.cleaned_data['name']
+              em = fm.cleaned_data['email']
+              pw = fm.cleaned_data['password']
+              reg = User(name=nm,email=em,password=pw)
+              fm.save()        
+              fm=StudentReg()
+
+
+      else:
+            fm=StudentReg()
+      stud =User.objects.all()
+      return render(request,'enroll/add_show.html',{'form':fm,'stu':stud})
+
+
+# delete :-
+def dele(request,id):
+ if request.method == "POST":
+       pi=User.objects.get(pk=id)
+       pi.delete() 
+       return HttpResponseRedirect('/')
+
+
+# update :-
+
+def update_data(request):
+      return render(request,'enroll/updates_stud.html')
