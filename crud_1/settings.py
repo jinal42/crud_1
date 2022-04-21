b277d5192b421ea032de3c823651b739430390ca
jinal42 <jij.globaliasoft@gmail.com>
2022-04-21 18:25:53 +0530

    crud

--- crud_1/settings.py
+++ crud_1/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'crud_1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'jinaldb',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- enroll/models.py
+++ enroll/models.py
@@ -1,5 +1,5 @@
 import email
-from pyexpat import model
+from pyexpat import model     
 from django.db import models
 
 # Create your models here.
